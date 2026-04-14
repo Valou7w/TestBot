fb7bf96f47958e6e3e3e8058edd2effbea63deef
Valentin <wurth.valentin@icloud.com>
2026-04-15 01:34:38 +0200

    Structuration du code, modification des commandes de moderation.

--- bot.py
+++ bot.py
@@ -4,40 +4,53 @@ from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
 
+#######################################################################################
+#                                Initialisation du bot                                #
+#######################################################################################
+
 print("Lancement du bot...")
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("Bot prêt.")
-    # Synchroniser les commandes qu'on a crées
+    # Synchronisation des commandes @bot.tree.command
     try:
-        #sync
         synced = await bot.tree.sync()
         print(f"Commandes synchronisées : {len(synced)}")
     except Exception as e:
         print(e)
 
+#######################################################################################
+#                           Réaction a un message specifique                          #
+#######################################################################################
+
 @bot.event
+# Empecher le bot de repondre à ses messages ou ceux d'un autre bot
 async def on_message(message: discord.Message):
-    # Empecher le bot de repondre a ses messages 
     if message.author.bot:
         return
-
-    if message.content.lower() == 'bienvenu':
+    
+# Le bot répond au message "bienvenue" dans le channel "1492876792090857583"
+    if message.content.lower() == "bienvenue":
         welcome_channel = bot.get_channel(1492876792090857583)
         await welcome_channel.send("Bienvenue chez DEDSEC")
 
-    if message.content.lower() == 'salut':
+# Le bot répond au message "salut" dans le channel d'envoi du message
+    if message.content.lower() == "salut":
         channel = message.channel
         await channel.send("Salut")
+
+#######################################################################################
+#                 Commandes de test a supprimer ou modifier plus tard                 #
+#######################################################################################
 
 @bot.tree.command(name="instagram", description=("Affiche mon Instagram"))
 async def instagram(interaction: discord.Interaction):
     await interaction.response.send_message("Voici mon Instagram : https://www.instagram.com/valou7w/")
 
-@bot.tree.command(name="test", description=("test embed"))
-async def test(interaction: discord.Interaction):
+@bot.tree.command(name="testembed", description=("Commande de test pour les messages de type 'embed'."))
+async def testembed(interaction: discord.Interaction):
     embed = discord.Embed(
         title="Titre test",
         description="Description test",
@@ -49,28 +62,28 @@ async def test(interaction: discord.Interaction):
     embed.set_author(name="DEDSEC")
     embed.set_image(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3M1eHp5cjByNHJtcDZodndpcng4d2JuaG4wM3VmdnlidHJpdWdyMiZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/GpyS1lJXJYupG/giphy.gif")
 
-
     await interaction.response.send_message(embed=embed)
 
 
 #######################################################################################
-############################### COMMANDES DE MODERATION ###############################
+#                               Commandes de moderation                               #
 #######################################################################################
 
 @bot.tree.command(name="warn", description=("Averti un utilisateur"))
 async def warn(interaction: discord.Interaction, member: discord.Member):
-    await interaction.response.send_message("Avertissement envoyé.")
+    await interaction.response.send_message(f"Avertissement envoyé à {member}", ephemeral=True)
     await member.send("Ceci est un avertissement, calme toi s'il te plait.")
 
 @bot.tree.command(name="ban", description=("Banni un utilisateur"))
 async def warn(interaction: discord.Interaction, member: discord.Member):
-    await interaction.response.send_message(f"{member} a été banni.")
+    await interaction.response.send_message(f"{member} a été banni.", ephemeral=True)
     await member.send("Tu as été banni.")
     await member.ban()
  
-@bot.tree.command(name="clear", description=("Supprime les 10 derniers messages"))
-async def clear(ctx, amount: int = 10):
-    await ctx.channel.purge(limit = amount)
+@bot.tree.command(name="clear", description=("Supprime autant de messages que défini (par defaut : 10)"))
+async def clear(interaction: discord.Interaction, amount: int = 10):
+    await interaction.response.send_message(f"{amount} messages ont été supprimés.", ephemeral=True)
+    await interaction.channel.purge(limit = amount)
 
 
 
